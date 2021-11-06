7ede1160a538c3f00c3f9a3f6543273978a485a9
Ray Lacerda <70419031+raylacerda@users.noreply.github.com>
2021-11-06 14:31:32 -0300

    V2

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1>Hola FIAP!</h1>\nMBA! o/ V2"
+    return "<h1>Hola FIAP!</h1>\nMBA! o/ Ray: V2"
 
 if __name__ == '__main__':
     # application.run()
